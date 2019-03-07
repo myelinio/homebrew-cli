d4cbbe1c39ae8cb7c8d83d35bfa85b97a7e255d5
tamasjambor <jamborta@gmail.com>
2019-03-07 01:19:19 +0000

    update script

--- Formula/myelin.rb
+++ Formula/myelin.rb
@@ -3,11 +3,11 @@ class Myelin < Formula
   homepage "https://myelin.io"
   url "https://myelin-cli.storage.googleapis.com/cli-darwin/myelin-v0.1.7.tar.gz"
   version "v0.1.7"
-  sha256 "c0955d1979de8a48268677d1e8bababb54ea6d885e5349acec374e3e7c86c572"
+  sha256 "fc938baa497f6a65924425d475b7f9eb7ab7d96c1b348a06bfe4c9bd53e64648"
 
   def install
-    bash_completion.install "dist/myelin-completion.bash" => "myelin"
-    zsh_completion.install "dist/myelin-completion.zsh" => "_myelin"
-    bin.install "dist/myelin-darwin-amd64" => "myelin"
+    bash_completion.install "myelin-completion.bash" => "myelin"
+    zsh_completion.install "myelin-completion.zsh" => "_myelin"
+    bin.install "myelin-darwin-amd64" => "myelin"
   end
 end
