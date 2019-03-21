368b3c25a5b0a5640f52871dfb9f7c020c243415
tamasjambor <jamborta@gmail.com>
2019-03-21 15:22:20 +0000

    update bash comp

--- Formula/myelin.rb
+++ Formula/myelin.rb
@@ -3,7 +3,7 @@ class Myelin < Formula
   homepage "https://myelin.io"
   url "https://myelin-cli.storage.googleapis.com/cli-darwin/myelin-v0.2.1.tar.gz"
   version "v0.2.1"
-  sha256 "ec7e0763d6ff783b706ecaaa56f394c78447a9348c25604c38fdf857ffc13d2e"
+  sha256 "2c68365f71b9a82ba4a2fe7bfcbdb63ae6f83c81b11fdfde6c7dab54575c52f5"
 
   def install
     bash_completion.install "myelin-completion.bash" => "myelin"
