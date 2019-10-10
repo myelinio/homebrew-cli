e26be5a292029147f32b31e14b941b862e57928a
tamasjambor <jamborta@gmail.com>
2019-10-10 13:14:04 +0100

    add default version

---
+++ Formula/myelin.rb
@@ -0,0 +1,13 @@
+class Myelin < Formula
+  desc "Myelin.io command line interface"
+  homepage "https://myelin.io"
+  url "https://myelin-cli.storage.googleapis.com/cli-darwin/myelin-v0.2.5.tar.gz"
+  keg_only :versioned_formula
+  sha256 "8df03454c00d39fc9614160c814d99d2b10575d2360ccd252f5b6b6e884463d3"
+
+  def install
+    bash_completion.install "myelin-completion.bash" => "myelin"
+    zsh_completion.install "myelin-completion.zsh" => "_myelin"
+    bin.install "myelin-darwin-amd64" => "myelin"
+  end
+end
