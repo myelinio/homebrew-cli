f431d925e1c20dc7d4ef52c29aa2465582ee748d
tamasjambor <jamborta@gmail.com>
2019-10-10 13:17:45 +0100

    add default version

--- Formula/myelin@0.2.5.rb
+++ Formula/myelin@0.2.5.rb
@@ -2,7 +2,6 @@ class MyelinAT025 < Formula
   desc "Myelin.io command line interface"
   homepage "https://myelin.io"
   url "https://myelin-cli.storage.googleapis.com/cli-darwin/myelin-v0.2.5.tar.gz"
-  keg_only :versioned_formula
   sha256 "8df03454c00d39fc9614160c814d99d2b10575d2360ccd252f5b6b6e884463d3"
 
   def install
