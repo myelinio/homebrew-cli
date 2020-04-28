8038f4a115ebb5200bcd0873d280657a59c6f9c5
Tamas Jambor <jamborta@gmail.com>
2020-04-29 00:08:31 +0100

    Update myelin@0.4.0.rb

--- Formula/myelin@0.4.0.rb
+++ Formula/myelin@0.4.0.rb
@@ -2,7 +2,7 @@ class MyelinAT040 < Formula
   desc "Myelin.io command line interface"
   homepage "https://myelin.io"
   url "https://myelin-cli.storage.googleapis.com/cli-darwin/myelin-0.4.0.tar.gz"
-  sha256 "dcdb7fcd9936c6b61ddc851c51aae2dfeeb4ddeac9f207be545ed23b00a8a779"
+  sha256 "c932e62e38c2925269bffb5e613d1af0caea4a14d039016528bbd361c9a8660b"
 
   def install
     bash_completion.install "myelin-completion.bash" => "myelin"
