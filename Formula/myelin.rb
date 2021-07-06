2a2d102b26e0bb315fe3ad815b4ac1fe4472d8fb
Tamas Jambor <Tamas.Jambor@kindredgroup.com>
2021-07-06 23:17:45 +0100

    0.4.6 - improved zsh and bash completion

--- Formula/myelin.rb
+++ Formula/myelin.rb
@@ -2,7 +2,7 @@ class Myelin < Formula
   desc "Myelin.io command line interface"
   homepage "https://myelin.io"
   url "https://myelin-cli.storage.googleapis.com/cli-darwin/myelin-0.4.6.tar.gz"
-  sha256 "c3848e48f605e059e47975e3390bead17447a1ecec5352a838935e00801527b9"
+  sha256 "0334d4a041e140d85b0c6919b14551f077a0bbabf366ec854777efbc2a69f79c"
 
   def install
     bash_completion.install "myelin-completion.bash" => "myelin"
